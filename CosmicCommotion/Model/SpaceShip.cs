ec8af46864136203cc2bacfb9aba8f27e01858c5
Kexs <akexsg@gmail.com>
2024-04-13 22:50:10 +0500

    Тесты, методы для корабля

--- CosmicCommotion/Model/SpaceShip.cs
+++ CosmicCommotion/Model/SpaceShip.cs
@@ -1,27 +1,102 @@
-﻿namespace CosmicCommotion.Model
+﻿using System;
+using System.Windows.Forms;
+
+public class SpaceShip
 {
-    public class SpaceShip
+    public int X { get; set; }
+    public int Y { get; set; }
+    public int Speed { get; set; }
+    public int Health { get; set; }
+    public bool IsShieldActive { get; private set; }
+    private Timer shieldTimer;
+    public int Score { get; set; }
+    public int Level { get; private set; }
+    public bool IsDestroyed { get; private set; }
+
+    public SpaceShip(int x, int y, int speed, int health)
     {
-        public int X { get; set; }
-        public int Y { get; set; }
-        public int Speed { get; set; }
-        public int Health { get; set; }
+        X = x;
+        Y = y;
+        Speed = speed;
+        Health = health;
+        IsShieldActive = false;
+        Level = 1;
+        IsDestroyed = false;
+        // Создаем и настраиваем таймер для щита
+        shieldTimer = new Timer();
+        shieldTimer.Interval = 1000; // Интервал в миллисекундах (здесь 1 секунда)
+        shieldTimer.Tick += new EventHandler(ShieldTimer_Tick); // Подключаем обработчик события Tick
+    }
 
-        public SpaceShip(int x, int y, int health)
+    public void MoveUp()
+    {
+        Y -= Speed;
+    }
+
+    public void ActivateShield()
+    {
+        if (!IsShieldActive)
         {
-            X = x;
-            Y = y;
-            Health = health;
+            IsShieldActive = true;
+            shieldTimer.Start(); // Запускаем таймер при активации щита
+        }
+    }
+
+    private void ShieldTimer_Tick(object sender, EventArgs e)
+    {
+        IsShieldActive = false; // Выключаем щит по истечении времени таймера
+        shieldTimer.Stop(); // Останавливаем таймер
+    }
+
+    public void Update()
+    {
+        if (IsShieldActive)
+        {
+            // Логика для активного щита
         }
 
-        public void MoveUp()
+        // Логика обновления состояния корабля
+        // Например, проверка столкновений, перемещение или другие действия
+    }
+
+    public void Repair()
+    {
+        Health = 100;
+    }
+
+    public void CollectStar()
+    {
+        Score += 100;
+        if (Score >= 500)
         {
-            
+            Level++;
+            Score = 0;
         }
-        
-        public void MoveDown()
+    }
+    
+    public void ApplyDamage(int damage)
+    {
+        Health -= damage;
+        if (Health <= 0)
         {
-            
+            Destroy();
         }
+    }
+
+    private void Destroy()
+    {
+        IsDestroyed = true;
+    }
+
+    public bool CheckCollision(SpaceShip otherShip)
+    {
+        // Проверяем столкновение с другим кораблем по координатам
+        return X == otherShip.X && Y == otherShip.Y;
+    }
+
+    public bool CheckOutOfBounds(int maxX, int maxY)
+    {
+        // Проверяем, находится ли корабль за пределами игрового поля
+        return X < 0 || X >= maxX || Y < 0 || Y >= maxY;
     }
 }
---
+++ Test/Tests.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using CosmicCommotion.Model;
+using NUnit.Framework;
+
+namespace Test
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+    
+    [TestFixture]
+    public class SpaceShipGameTests
+    {
+        [Test]
+        public void TestShipMovesUp()
+        {
+            SpaceShip ship = new SpaceShip(0, 0, 1, 3);
+
+            ship.MoveUp();
+
+            Assert.AreEqual(0, ship.X);
+            Assert.AreEqual(-1, ship.Y);
+        }
+
+        [Test]
+        public void TestShipCollectsStar()
+        {
+            SpaceShip ship = new SpaceShip(0, 0, 1, 3);
+            int initialScore = 0;
+        
+            ship.CollectStar();
+            int updatedScore = ship.Score;
+        
+            Assert.AreEqual(initialScore + 100, updatedScore);
+        }
+
+        [Test]
+        public void TestShipHitByAsteroid()
+        {
+            SpaceShip ship = new SpaceShip(0, 0, 1, 3);
+            int initialHealth = ship.Health;
+
+            ship.ApplyDamage(2);
+            int updatedHealth = ship.Health;
+
+            Assert.AreEqual(initialHealth - 2, updatedHealth);
+        }
+        
+        [Test]
+        public void TestGameLevelIncreases()
+        {
+            SpaceShip ship = new SpaceShip(0, 0, 1, 3);
+            int initialLevel = 1;
+
+            ship.CollectStar();
+            int updatedLevel = ship.Level;
+
+            Assert.AreEqual(initialLevel, updatedLevel);
+        }
+
+        [Test]
+        public void TestGameEndsWhenShipDestroyed()
+        {
+            SpaceShip ship = new SpaceShip(0, 0, 1, 3);
+            
+            ship.ApplyDamage(3);
+
+            Assert.IsTrue(ship.IsDestroyed);
+        }
+    }
+}
